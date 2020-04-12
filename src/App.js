5e99577f1d378a5d6c57b1106ad9abd79fb26a23
Vu Thanh Ha <vuha98.tn@gmail.com>
2020-04-12 23:56:46 +0700

    [1.0.0] Delete Task

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import Header from './components/Header'
 import AddTask from './components/AddTask'
 import ListTask from './components/ListTask'
 
+
 export default class App extends Component {
+
     render() {
         return (
             <div className="app">
