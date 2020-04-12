0d8caefe6cc55fb0d0cbeff3d16c8732aea60ffe
Vu Thanh Ha <vuha98.tn@gmail.com>
2020-04-12 21:52:53 +0700

    [1.0.0] Add React Redux

--- src/components/ListTask.js
+++ src/components/ListTask.js
@@ -1,8 +1,28 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import {fetchTasks} from './../actions/taskAction';
+
 import Task from './Task'
 
-export default class ListTask extends Component {
+class ListTask extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+        }
+    }
+
+    componentWillMount() {
+        this.props.fetchTasks();
+    }
+
     render() {
+        const {tasks} = this.props;
+        const listTask = tasks.map(task => (
+            <Task key={task.id} task={task}/>
+        ));
+
         return (
             <div className="list-task">
                 <div className="row">
@@ -10,10 +30,34 @@ export default class ListTask extends Component {
                         <h6 className="title">List Task</h6>
                     </div>
                     <div className="col-md-12">
-                        <Task/>
+                        {listTask}
                     </div>
                 </div>
             </div>
         )
     }
 }
+
+ListTask.propTypes = {
+    tasks: PropTypes.array.isRequired,
+    fetchTasks: PropTypes.func,
+}
+
+const mapStateToProps = (state) => ({
+    tasks: state.task.items
+});
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        fetchTasks: () => {
+            fetch('http://localhost:80/tasks')
+            .then(res => res.json())
+            .then(tasks => {
+                dispatch(fetchTasks(tasks));
+            })
+        }
+    }
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(ListTask);
